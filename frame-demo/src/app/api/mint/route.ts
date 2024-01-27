65648c35fa592306270c5db683edb2168bee1470
Rob Polak <rob.polak@coinbase.com>
2024-01-27 10:43:17 -0600

    Neynar integration

--- frame-demo/src/app/api/mint/route.ts
+++ frame-demo/src/app/api/mint/route.ts
@@ -1,10 +1,35 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
-
+const axios = require('axios');
 const client = getSSLHubRpcClient("nemes.farcaster.xyz:2283");
+
+interface fidResponse {
+  verifications: string[];
+}
+async function getAddrByFid(fid: number) {
+  const options = {
+    method: 'GET',
+    url: `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
+    headers: {accept: 'application/json', api_key: 'NEYNAR_API_DOCS'}
+  };
+  const resp = await axios.get(options.url, {headers: options.headers});
+  if (resp.data?.users) {
+    const userVerifications = resp.data.users[0] as fidResponse;
+    if(userVerifications.verifications) {
+      return userVerifications.verifications[0]
+    }
+  }
+  return "0x00";
+}
+
+export async function GET(req: NextRequest): Promise<Response> {
+  const addr = await getAddrByFid(194519);
+  return new NextResponse(JSON.stringify(addr))
+}
+
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let validatedMessage: Message | undefined = undefined;
-  let signer: `0x${string}` | undefined = undefined;
+  let signer: string = "";
   let fid= 0;
   try {
     const body = await req.json();
@@ -12,10 +37,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const result = await client.validateMessage(frameMessage);
     if (result.isOk() && result.value.valid && result.value.message) {
       validatedMessage = result.value.message;
-      signer = '0x' + Buffer.from(result.value.message.signer).toString('hex')
-
     }
     fid = validatedMessage?.data?.fid || 0;
+    signer = await getAddrByFid(fid);
   } catch (err) {
     console.error(err);
   }
@@ -30,7 +54,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
               <meta name="fc:frame" content="vNext">
               <meta name="fc:frame:image" content="https://frame-demo.vercel.app/success.png">
               <meta name="fc:frame:post_url" content="https://frame-demo.vercel.app/api/mint">
-              <meta name="fc:frame:button:1" content="${fid}">
+              <meta name="fc:frame:button:1" content="${signer}">
             </head>
             <body>
               <p>Testing Complete</p>
