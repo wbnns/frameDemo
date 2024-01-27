569316ea3b3457c29654f72d635b7c79886840b6
Rob Polak <rob.polak@coinbase.com>
2024-01-27 09:56:07 -0600

    Add message validation

--- frame-demo/src/app/api/mint/route.ts
+++ frame-demo/src/app/api/mint/route.ts
@@ -1,6 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
 
-function getResponse(): NextResponse {
+const client = getSSLHubRpcClient("nemes.farcaster.xyz:2283");
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let validatedMessage: Message | undefined = undefined;
+  let signer: `0x${string}` | undefined = undefined;
+  try {
+    const body = await req.json();
+    const frameMessage = Message.decode(Buffer.from(body?.trustedData?.messageBytes || '', 'hex'));
+    const result = await client.validateMessage(frameMessage);
+    if (result.isOk() && result.value.valid && result.value.message) {
+      validatedMessage = result.value.message;
+      signer = '0x' + Buffer.from(result.value.message.signer).toString('hex')
+
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
   return new NextResponse(`
       <!DOCTYPE html>
           <html>
@@ -11,7 +28,7 @@ function getResponse(): NextResponse {
               <meta name="fc:frame" content="vNext">
               <meta name="fc:frame:image" content="https://frame-demo.vercel.app/success.png">
               <meta name="fc:frame:post_url" content="https://frame-demo.vercel.app/api/mint">
-              <meta name="fc:frame:button:1" content="Tested">
+              <meta name="fc:frame:button:1" content="${signer}">
             </head>
             <body>
               <p>Testing Complete</p>
