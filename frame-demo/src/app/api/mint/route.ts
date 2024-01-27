db45a8e57506eaef797e11be4d56022f913d19e5
Rob Polak <rob.polak@coinbase.com>
2024-01-27 09:12:18 -0600

    updating api

--- frame-demo/src/app/api/mint/route.ts
+++ frame-demo/src/app/api/mint/route.ts
@@ -1,13 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+function getResponse() {
+  return new NextResponse(`
+      <!DOCTYPE html>
+          <html>
+            <head>
+              <title>Vote Recorded</title>
+              <meta property="og:title" content="Tested!">
+              <meta property="og:image" content="https://frame-demo.vercel.app/mint.png">
+              <meta name="fc:frame" content="vNext">
+              <meta name="fc:frame:image" content="https://frame-demo.vercel.app/api/mint">
+              <meta name="fc:frame:post_url" content="https://frame-demo.vercel.app/mint.png">
+              <meta name="fc:frame:button:1" content="Tested">
+            </head>
+            <body>
+              <p>Testing Complete</p>
+            </body>
+          </html>
+      
+      `)
+}
+export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse();
+}
 
 export async function GET(req: NextRequest): Promise<Response> {
-    return NextResponse.json({ hello:"world" }, { status: 200 });
+  return getResponse();
 }
-
-export async function POST(req: NextRequest): Promise<Response> {
-    return NextResponse.json({ hello:"world" }, { status: 200 });
-}
-
 
 export const dynamic = 'force-dynamic';
