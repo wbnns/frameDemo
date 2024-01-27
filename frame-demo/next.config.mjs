447fac495352f3cc77380000fdfcb82016fb1bbd
Rob Polak <rob.polak@coinbase.com>
2024-01-27 08:56:27 -0600

    allow cors

--- frame-demo/next.config.mjs
+++ frame-demo/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ]
+      }
+    ]
+  }
+};
 
 export default nextConfig;
